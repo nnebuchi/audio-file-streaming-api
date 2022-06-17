1b3e8300210cea50bb838d818bb58d41c1bede0e
Nnebuchi <nnebuchiosigbo340@gmail.com>
2022-06-17 12:34:08 +0100

    fixed otp validation error response to be same format ith regostration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -73,12 +73,17 @@ class AuthController extends Controller
 
 
         if ($validator->fails()) {
-           
-            return json_encode([
-                'status'=>'fail',
-                'message'=>'Request Failed'
-                // 'errors'=>$validator->errors()
-            ]); // Status code here
+            $errs = json_decode($validator->errors(), true);
+            // return $validator->errors();
+            foreach($errs as $key=>$err){
+                return json_encode([
+                    'status'    => 'fail',
+                    'message'   => 'Registration Failed',
+                    'error'     =>  $err[0]
+                ]); // Status code here
+                break;
+            }
+            
            
         }
         AuthService::verifyOTP(sanitize_input($request->otp), sanitize_input($request->email));
