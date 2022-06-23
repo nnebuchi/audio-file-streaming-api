e248befc9aad7e005f2d80f46225402c4b181fd4
Nnebuchi <nnebuchiosigbo340@gmail.com>
2022-06-23 10:56:51 +0100

    removed token and user property from signup response and added is_verified property

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -19,14 +19,11 @@ class AuthService
         $user->save();
 
         self::sendOTP($user);
-        $token = $user->createToken('auth_token')->plainTextToken;
+        
         return json_encode([
             'status'    => 'success',
             'email'     => $user->email,
-            'user'      => $user,
-            'token'     => $token,
             'message'   => 'registration successful. Check your email for your account verification code',
-
         ], 200);
     }
 
@@ -53,7 +50,14 @@ class AuthService
     public static function login($email, $password){
         // the DB class was used instead of User Model because we needed to access the pasword property hich is hidden on the User Model
         $user = DB::table('users')->where('email',$email)->first();
-        
+        if($user && (is_null($user->otp || $user->otp==''))){
+            return json_encode([
+                'status'        => 'success',
+                'message'       => 'Login failed',
+                'error'         => 'Email not verified',
+                'is_verified'   => false  
+            ]);
+        }
         if($user && Hash::check($password, $user->password)){
            return self::authenticate(($user->email));
         }
@@ -69,9 +73,10 @@ class AuthService
         $user = User::where('email', $email)->first();
         $token = $user->createToken('auth_token')->plainTextToken;
             return json_encode([
-                'status'    => 'success',
-                'message'   => 'verification successful',
-                'token'     =>  $token
+                'status'        => 'success',
+                'message'       => 'verification successful',
+                'token'         =>  $token,
+                'is_verified'   => true 
             ]); // Status code here
     }
     
