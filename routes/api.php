93b3149bdd1905c6c9d8ba22991a7fbfdceedf01
Nnebuchi <nnebuchiosigbo340@gmail.com>
2022-07-12 16:48:19 +0100

    imported files conroller

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserApiController;
 use App\Http\Controllers\PublishersController;
+use App\Http\Controllers\FilesController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
