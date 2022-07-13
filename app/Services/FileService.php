4856f0c4a369f19e0362c7bf3af3d7e236bcb3aa
Nnebuchi <nnebuchiosigbo340@gmail.com>
2022-07-13 16:09:29 +0100

    kkdjflksdk

--- app/Services/FileService.php
+++ app/Services/FileService.php
@@ -31,7 +31,7 @@ class FileService{
             $files =$files->latest();
         }
         
-        $files = $request->publishers ? $files->inRandomOrder()->limit(50)->get(['title','file', 'creator:firstname, lastname']) : $files->paginate(50);
+        $files = $request->publishers ? $files->inRandomOrder()->limit(50)->get() : $files->paginate(50);
         
         return Response::json([
             'status'    => 'success',
