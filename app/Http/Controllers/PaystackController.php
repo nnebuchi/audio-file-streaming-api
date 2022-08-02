b8fbddf15a68edc677467fe1a353b64e5c75a55d
Nnebuchi <nnebuchiosigbo340@gmail.com>
2022-08-02 14:59:00 +0100

    added reference to et604 paystacks tables

--- app/Http/Controllers/PaystackController.php
+++ app/Http/Controllers/PaystackController.php
@@ -11,6 +11,12 @@ class PaystackController extends Controller
         // return json_encode($request->all());
         $paystack = new Paystack;
         $paystack->dump = json_encode($request->all());
+        
+
+        if($request->reference){
+            $paystack->reference = $request->reference;
+
+        }
         $paystack->save();
         return 'done';
     }
