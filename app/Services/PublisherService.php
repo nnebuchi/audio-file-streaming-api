52e839fa8cab46705f08a4131cfc24b6f0847c8b
Nnebuchi <nnebuchiosigbo340@gmail.com>
2022-08-24 12:37:03 +0100

    pushing recent messages endpoints

--- app/Services/PublisherService.php
+++ app/Services/PublisherService.php
@@ -5,6 +5,7 @@ use App\Models\Listen;
 use App\Models\User;
 use App\Models\AudioFile;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class PublisherService{
     public static function get(){
@@ -30,6 +31,13 @@ class PublisherService{
     public static function getTrendingPublishers(){
 
         $pubs = Creator::select('firstname', 'lastname', 'profile_pic', 'logo', 'public_name')->withCount('listens')->orderByDesc('listens_count');
+
+        // $pubs = DB::table('creators')
+            
+        //     ->leftJoin('audio_files', 'audio_files.creator_id', '=', 'creators.id')
+        //     ->leftJoin('listens', 'audio_files.id', '=', 'listens.audio_file_id')
+        //     ->select('creators.firstname', 'creators.lastname', 'creators.profile_pic', 'creators.logo', 'creators.public_name');
+           
 
         return json_encode([
             'status'    => 'success',
